22d621dc41b0f1b43bb76e740e4769ac1a189f6e
asobrabj <j.barbosasilva@yahoo.com.br>
2023-03-29 20:58:40 -0300

    Primeiro commit - Projeto em andamento - clinica estetica

---
+++ projeto/src/old/js/index.js
@@ -0,0 +1,19 @@
+import '../scss/styles.scss'
+import imageBuilding from '../assets/building.jpg'
+import imageNotebook from '../assets/notebook.jpg'
+
+
+
+const elemImageBuilding = new Image()
+elemImageBuilding.src = imageBuilding
+
+const elemImageNotebook = new Image()
+elemImageNotebook.src = imageNotebook
+
+/*
+document.querySelector('#building')
+    .appendChild(elemImageBuilding)
+
+document.querySelector('#notebook')
+    .appendChild(elemImageNotebook)
+    */
